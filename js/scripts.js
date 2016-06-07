ebfc16ae3c25befe7bdead1c8f898bb8d341eb10
addison <addisonlane@me.com>
2016-06-07 14:07:07 -0700

    Fix string replace code to be a simple method and not a regular expression

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 /* Word Puzzle */
 
+
 $(function(){
 
 var sentence = "Believe you can and you're halfway there. Theodore Roosevelt";
@@ -8,32 +9,43 @@ var vowels = ["a", "e", "i", "o", "u"];
 console.log(sentence);
 console.log(vowels);
 
-var sentSplit = sentence.split("");
-console.log(sentSplit);
+/* Below is the regular expression of what needs to happen:
+var fixed = sentence.replace(/e/g, "-").replace(/a/g, "-").replace(/i/g, "-").replace(/o/g, "-").replace(/u/g, "-");
+*/
 
+puzzle = sentence.replace(/([aeiou])/g, function (match) {
+    return match.replace(match, "-");
+});
 
-var array_sentence_keys = new Array();
-var array_sentence_values = new Array();
-for (var key in sentSplit) {
-  array_sentence_keys.push(key);
-  array_sentence_values.push(sentSplit[key]);
-}
+$("#puzzle").text(puzzle);
 
-var array_vowels_keys = new Array();
-var array_vowels_values = new Array();
-for (var key in vowels) {
-  array_vowels_keys.push(key);
-  array_vowels_values.push(vowels[key]);
-}
+// var letters = sentence.split("");
+// console.log(letters);
 
-console.log(array_sentence_keys);
-console.log(array_sentence_values);
-console.log(array_vowels_keys);
-console.log(array_vowels_values);
+// var array_sentence_keys = new Array();
+// var array_sentence_values = new Array();
+// for (var key in letters) {
+//   array_sentence_keys.push(key);
+//   array_sentence_values.push(letters[key]);
+// }
+//
+// var array_vowels_keys = new Array();
+// var array_vowels_values = new Array();
+// for (var key in vowels) {
+//   array_vowels_keys.push(key);
+//   array_vowels_values.push(vowels[key]);
+// }
+//
+// console.log(array_sentence_keys);
+// console.log(array_sentence_values);
+// console.log(array_vowels_keys);
+// console.log(array_vowels_values);
+//
+//
+// for (var index = 0; index < letters.length; index +=1){
+//   $("#puzzle").append(letters[index]);
+// }
 
-for (var index = 0; index < sentSplit.length; index +=1){
-  $("#puzzle").append(sentSplit[index]);
-}
 
 
 
